2ea1f131a8df0c969d5033b7cc887d2e615a84c7
oneilld136 <oneilld136@gmail.com>
2019-04-21 00:54:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+array = ["rat", "fang", true, "rat"]
+def begins_with_r(array)
+  array.all? {|word| word.start_with?("r")}
+end
+
+begins_with_r(array)
+
+def contain_a(array)
+	array.find_all {|word| word.include?("a")}
+end
+
+contain_a(array)
+
+def first_wa(array)
+  array.find {|prefix| prefix.include?("wa")}
+end
+
+first_wa(array)
+
+def remove_non_strings(array)
+	array.delete_if {|string| string.class != String}
+end
+
+remove_non_strings(array)
+
+def count_elements(array)
+  array.count {|name| name > 0}
+end
 # your code goes here
